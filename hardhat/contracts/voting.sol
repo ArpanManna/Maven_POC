06aa0e034a361531cd5739ca651e480cc96bc013
Arpan Manna <arpan.manna113@gmail.com>
2023-07-09 07:34:06 +0530

    updated voting contract

--- hardhat/contracts/voting.sol
+++ hardhat/contracts/voting.sol
@@ -19,12 +19,17 @@ contract Voting {
         address[] clients;
 	}
 
-	mapping (uint => Vote) public projects;
+	mapping (uint => Vote) public disputedProjects; // mapping of disputed project Id to Vote Details
 	uint256 votingPeriodConstant = 60 seconds;
 
-    function initializeVoting(uint projectId) public {
-        require(projects[projectId].duration == 0, "Already Initialized!");
-        projects[projectId].duration = block.timestamp + votingPeriodConstant;
+    // @dev - Initial setup: voting_ballot -> 0: not eligible, 3: eligible for voting
+    function initializeVoting(uint projectId, address[] calldata toBeWhitelisted) public {
+        require(disputedProjects[projectId].duration == 0, "Already Initialized!");
+        require(toBeWhitelisted.length !=0, "Cannot Initialize : Empty voters list!");
+        for(uint i=0; i<toBeWhitelisted.length; i++){
+            disputedProjects[projectId].voting_ballot[toBeWhitelisted[i]] = 3;
+        }
+        disputedProjects[projectId].duration = block.timestamp + votingPeriodConstant;
         disputedProjectIds.push(projectId);
     }
 
@@ -32,34 +37,36 @@ contract Voting {
 	function vote(uint projectId, uint _vote) external {
         // to check if project Id is valid
         require(msg.sender != deployer, "This address not eligible for voting");
-        require(projects[projectId].duration != 0, "Voting Not Started Yet!");
-		require(projects[projectId].duration > block.timestamp, "Voting for this project is Over!");
-		require(projects[projectId].voting_ballot[msg.sender] == 0, "You can vote only once");
+        require(disputedProjects[projectId].duration != 0, "Voting Not Started Yet!");
+		require(disputedProjects[projectId].duration > block.timestamp, "Voting for this project is Over!");
+        //require(projects[projectId].voting_ballot[msg.sender] == 0, "Not eligible for voting");
+        require(disputedProjects[projectId].voting_ballot[msg.sender] == 3, "Either you are not eligible or You can vote only once");
+		
 		require(_vote == 1 || _vote == 2, "Invalid Voting");
         
         if(_vote == 1){
-            projects[projectId].votesFreelancer += 1;   // add vote in favor of freelancer
-            projects[projectId].freelancers.push(msg.sender);
+            disputedProjects[projectId].votesFreelancer += 1;   // add vote in favor of freelancer
+            disputedProjects[projectId].freelancers.push(msg.sender);
         }
         else if(_vote == 2){
-            projects[projectId].votesClient += 1;   // add vote in favor of client
-            projects[projectId].clients.push(msg.sender);
+            disputedProjects[projectId].votesClient += 1;   // add vote in favor of client
+            disputedProjects[projectId].clients.push(msg.sender);
         }
-		projects[projectId].voting_ballot[msg.sender] = _vote;
+		disputedProjects[projectId].voting_ballot[msg.sender] = _vote;
 	}
 
     // @dev - 0: unbiased, 1: freelancer, 2: client
 	function getVotingResult(uint projectId) public view returns (uint) {
-		require(projects[projectId].duration != 0, "Voting not started yet");
-		require(projects[projectId].duration < block.timestamp, "Voting not over yet!");
-        if(projects[projectId].votesFreelancer == projects[projectId].votesClient) return 0;
-		return projects[projectId].votesFreelancer > projects[projectId].votesClient ? 1 : 2;    
+		require(disputedProjects[projectId].duration != 0, "Voting not started yet");
+		require(disputedProjects[projectId].duration < block.timestamp, "Voting not over yet!");
+        if(disputedProjects[projectId].votesFreelancer == disputedProjects[projectId].votesClient) return 0;
+		return disputedProjects[projectId].votesFreelancer > disputedProjects[projectId].votesClient ? 1 : 2;    
 	}
 
     // @dev - this functions returns list of voters address for(freelancers and clients)
     function getVotingDetails(uint projectId) public view returns(address[] memory, address[] memory){
         //Vote storage vote =  projects[projectId];
-        return (projects[projectId].freelancers,projects[projectId].clients);
+        return (disputedProjects[projectId].freelancers,disputedProjects[projectId].clients);
     }
 
     // function getAllVotingDetails() public view returns(Vote[] memory){
@@ -75,7 +82,7 @@ contract Voting {
     function getResolvedDisputedCount() public view returns(uint){
         uint count = 0;
         for(uint i=0;i<disputedProjectIds.length;i++){
-            if(projects[disputedProjectIds[i]].votesFreelancer != projects[disputedProjectIds[i]].votesClient){
+            if(disputedProjects[disputedProjectIds[i]].votesFreelancer != disputedProjects[disputedProjectIds[i]].votesClient){
                 count += 1;
             }
         }
